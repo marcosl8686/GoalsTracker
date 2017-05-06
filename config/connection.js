11a6575f068c7fc55f1ae84f141e87c684ba1d22
Marcos Lee <marcosl8686@gmail.com>
2017-05-05 21:40:27 -0700

    add JAWSDB to connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "goals_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
